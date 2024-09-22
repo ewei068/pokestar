a9632d91e1d5729e3d84f8d0636d719838fd0a20
Elvis Wei <ewei068@berkeley.edu>
2024-09-21 20:25:28 -0700

    lint top-level

--- src/leaveServer.js
+++ src/leaveServer.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
+/* eslint-disable no-console */
+require("dotenv").config();
 
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { logger } = require('./log');
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { logger } = require("./log");
 
 // Create a new client instance
 const client = new Client({
-    intents:
-        [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildMessages,
-            // GatewayIntentBits.MessageContent
-        ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    // GatewayIntentBits.MessageContent
+  ],
 });
 
-client.once(Events.ClientReady, async c => {
-    logger.info(`Ready! Logged in as ${c.user.tag}`);
+client.once(Events.ClientReady, async (c) => {
+  logger.info(`Ready! Logged in as ${c.user.tag}`);
 
-    // leave server at an id
-    try {
-        await client.guilds.cache.get(process.argv[2]).leave();
-        console.log(`Left server ${process.argv[2]}`)
-    } catch (error) {
-        logger.error(error);
-    }
+  // leave server at an id
+  try {
+    await client.guilds.cache.get(process.argv[2]).leave();
+    console.log(`Left server ${process.argv[2]}`);
+  } catch (error) {
+    logger.error(error);
+  }
 });
 
 // Log in to Discord with your client's token
