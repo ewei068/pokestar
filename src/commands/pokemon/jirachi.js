730c636387b54159c4db9cdcfbccb12935f378b1
Elvis Wei <ewei068@berkeley.edu>
2025-01-23 00:04:39 -0800

    command config

---
+++ src/commands/pokemon/jirachi.js
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * @author Elvis Wei
+ *
+ * jirachi.js jirachi.
+ */
+const { buildDeoxysSend } = require("../../services/mythic");
+
+const deoxys = async (user) => await buildDeoxysSend(user);
+
+const deoxysMessageCommand = async (message) => {
+  const { send, err } = await deoxys(message.author);
+  if (err) {
+    await message.channel.send(`${err}`);
+    return { err };
+  }
+  await message.channel.send(send);
+};
+
+const deoxysSlashCommand = async (interaction) => {
+  const { send, err } = await deoxys(interaction.user);
+  if (err) {
+    await interaction.reply(`${err}`);
+    return { err };
+  }
+  await interaction.reply(send);
+};
+
+module.exports = {
+  message: deoxysMessageCommand,
+  slash: deoxysSlashCommand,
+};
