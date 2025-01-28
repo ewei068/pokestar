ce4401d2fbbe8dda80463bf34c6f59eb8fd279ef
Elvis Wei <ewei068@berkeley.edu>
2025-01-27 22:59:32 -0800

    tutorial stat analysis

---
+++ scripts/misc/getTutorialStats.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const fs = require("fs");
+const { collectionNames } = require("../../src/config/databaseConfig");
+const { newTutorialStages } = require("../../src/config/questConfig");
+const { countDocuments } = require("../../src/database/mongoHandler");
+
+const data = {};
+
+const getTutorialStats = async () => {
+  const lastMonthTime = new Date(
+    new Date().getTime() - 1000 * 60 * 60 * 24 * 30
+  ).getTime();
+  const totalUsersThisMonth = await countDocuments(collectionNames.USERS, {
+    lastCorrected: {
+      $gte: lastMonthTime,
+    },
+  });
+  data.total = totalUsersThisMonth;
+  for (const tutorialStage of newTutorialStages) {
+    const totalUsers = await countDocuments(collectionNames.USERS, {
+      [`tutorialData.completedTutorialStages.${tutorialStage}`]: true,
+      lastCorrected: {
+        $gte: lastMonthTime,
+      },
+    });
+    data[tutorialStage] = totalUsers;
+  }
+
+  console.log(data);
+
+  // save to csv
+  const csv = Object.entries(data)
+    .map(([key, value]) => `${key},${value}`)
+    .join("\n");
+  fs.writeFileSync("tutorialStats.csv", csv);
+};
+
+getTutorialStats();
