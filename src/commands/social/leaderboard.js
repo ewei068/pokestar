e3c091823b68c5be2f5daf1e54081c37141b321a
Elvis Wei <ewei068@berkeley.edu>
2025-05-26 10:47:15 -0700

    leaderboard rework

--- src/commands/social/leaderboard.js
+++ src/commands/social/leaderboard.js
@@ -1,81 +1,57 @@
 /**
  * @file
  * @author Elvis Wei
- * @date 2023
- * @section Description
  *
  * leaderboard.js is used as a way to interact with users more and give users a goal. It displas the top 10 leaderboard for a given category and scope.
  */
-const { getLeaderboard } = require("../../services/social");
-const { leaderboardConfig } = require("../../config/socialConfig");
-const { buildLeaderboardEmbed } = require("../../embeds/socialEmbeds");
+const { createRoot, userTypeEnum } = require("../../deact/deact");
+const Leaderboard = require("../../elements/social/Leaderboard");
 
 /**
- * Displays a top 10 leaderboard for a given category and scope.
- * @param {String} category Category to display leaderboard for.
- * @param {String} scope Scope of leaderboard (global, server).
- * @returns Embed with leaderboard.
+ * Creates and returns a Deact root for the leaderboard.
+ * @param {any} interaction The interaction (message or slash command)
+ * @param {string} initialCategory Initial category for the leaderboard
+ * @param {string} initialScope Initial scope for the leaderboard
+ * @param {any} guild Guild object for server-scoped leaderboards
+ * @returns {Promise<any>} The result of createRoot
  */
-const leaderboard = async (category, scope, guild) => {
-  const categoryData = leaderboardConfig[category];
-
-  // if scope = server, get guild members
-  // TODO: see if we can use non-cache version
-  let subset = null;
-  if (scope === "server") {
-    const members = await guild.members.cache;
-    subset = members.map((member) => member.user.id);
-  }
-
-  // get leaderboard
-  const leaderboardResult = await getLeaderboard(categoryData, subset);
-  if (leaderboardResult.err) {
-    return { embed: null, err: leaderboardResult.err };
-  }
-
-  // build leaderboard embed
-  const embed = buildLeaderboardEmbed(
-    leaderboardResult.data,
-    categoryData,
-    scope
+const leaderboard = async (interaction, initialCategory, initialScope, guild) =>
+  createRoot(
+    Leaderboard,
+    {
+      initialCategory,
+      initialScope,
+      guild,
+    },
+    interaction,
+    {
+      userIdForFilter: userTypeEnum.ANY,
+    }
   );
-
-  const send = {
-    embeds: [embed],
-  };
-
-  return { send, err: null };
-};
 
 const leaderboardMessageCommand = async (message, client) => {
   const args = message.content.split(" ");
-  const category = args[1];
+  const category = args[1] || "level"; // Default to level
   const scope = args[2] || "global"; // default to global
-  const { send, err } = await leaderboard(
+
+  return await leaderboard(
+    message,
     category,
     scope,
     client.guilds.cache.get(message.guild.id)
   );
-  if (err) {
-    await message.channel.send(`${err}`);
-    return { err };
-  }
-  await message.channel.send(send);
 };
 
 const leaderboardSlashCommand = async (interaction, client) => {
-  const category = interaction.options.getString("category");
+  const category = interaction.options.getString("category") || "level"; // Default to level
   const scope = interaction.options.getString("scope") || "global"; // default to global
-  const { send, err } = await leaderboard(
+
+  return await leaderboard(
+    interaction,
     category,
     scope,
     client.guilds.cache.get(interaction.guild.id)
   );
-  if (err) {
-    await interaction.reply(`${err}`);
-    return { err };
-  }
-  await interaction.reply(send);
 };
 
 module.exports = {
