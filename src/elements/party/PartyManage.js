f736a15ad34df7bc3d8f2fc9e42a82283adc5695
Elvis Wei <ewei068@berkeley.edu>
2025-06-10 21:07:37 -0700

    party manage stub

---
+++ src/elements/party/PartyManage.js
@@ -0,0 +1,15 @@
+/**
+ * Party Manage element for interactive party management
+ * @module elements/party/PartyManage
+ */
+const { createElement } = require("../../deact");
+
+/**
+ * Renders a party management interface
+ * @param {DeactElement} ref
+ * @param {object} props - Element props
+ * @param {CompactUser} props.user - User
+ */
+const PartyManage = async (ref, props) => ({});
+
+module.exports = PartyManage;
